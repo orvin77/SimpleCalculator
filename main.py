1cf63473389e8adafe91088f2e11beacff5fb069
Orvin Ahmed <orvinahmed134@gmail.com>
2024-06-28 14:53:47 -0500

    Adds calculate function
    The function takes in the equation from the user as a string.
    Parses the string and returns the result of the equation.
    
    Signed-off-by: Orvin Ahmed <orvinahmed134@gmail.com>

--- main.py
+++ main.py
@@ -1,22 +1,59 @@
+def calculate(equation):
+    i = 0
+    cur_num = 0
+    operation = "+"
+    result = 0 
+    prev = 0
+
+    while i < len(equation):
+        # parse string
+        while i < len(equation) and equation[i].isnumeric():
+            cur_num = cur_num * 10 + int(equation[i])
+            i += 1
+        if operation == "+":
+            result += cur_num
+        elif operation == "-":
+            result -= cur_num
+        elif operation == "*":
+            result *= cur_num
+        elif operation == "/":
+            result /= cur_num
+        
+        if i < len(equation):
+            operation = equation[i] 
+        
+        cur_num = 0
+
+        i += 1
+
+    return result
+
 def main():
     print("Welcome to the simple calculator!")
-
-    num1 = float(input("Enter a value: "))
-    num2 = float(input("Enter another value: "))
-    operation = input("Enter an operation(+, -, *, /): ")
-
-    if operation == "+":
-        result = num1 + num2
-    elif operation == "*":
-        result = num1 * num2
-    elif operation == "-":
-        result = num1 - num2
-    elif operation == "/":
-        result = num1 / num2
-    else:
-        print("Invalid operation was inputted.")
+    equation = input ("Enter an equation to be solved: ")
+    print(f"The equation is: {equation}")    
+    result = calculate(equation)
     
-    print(f"{num1} {operation} {num2} = {result}")
+    print(result)
+
+        
+    #num1 = float(input("Enter a value: "))
+    #num2 = float(input("Enter another value: "))
+    #operation = input("Enter an operation(+, -, *, /): ")
+
+    #if operation == "+":
+    #    result = num1 + num2
+    #elif operation == "*":
+    #    result = num1 * num2
+    #elif operation == "-":
+    #    result = num1 - num2
+    #elif operation == "/":
+    #    result = num1 / num2
+    #else:
+    #    print("Invalid operation was inputted.")
+    
+    #print(f"{num1} {operation} {num2} = {result}")
+
 
 if __name__ == "__main__":
     main()
